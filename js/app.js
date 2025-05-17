59e5e60370b63d9136e8dd29f10f01d142051d31
Arthur-Amaral-cloud <arthur.asilva@sptech.school>
2025-05-17 11:42:56 -0300

    Estruturação do sudoku e Troca de logos

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
     localStorage.setItem('darkmode', isDarkMode);
 });
 
+const cells = document.querySelector('.main-grid-cell')
+
 const name_input = document.querySelector('#input-name');
 const start_screen = document.querySelector('#start-screen');
 
@@ -33,6 +35,26 @@ document.querySelector('#btn-play').addEventListener('click', () => {
 
 const getGameInfo = () => JSON.parse(localStorage.getItem('game')); 
 
+//
+const initGameGrid = () => {
+    let index = 0;
+
+    for(let i = 0; i < Math.pow(CONSTANT.GRID_SIZE,2); i++)
+    {
+        let row = Math.floor(i / CONSTANT.GRID_SIZE);
+        let col = i % CONSTANT.GRID_SIZE;
+        if(row === 2 || row === 5)
+        {
+            cells[index].style.marginBottom = '10px';
+        } 
+       // if(col === 2 || col === 5) 
+      //  {
+       //     cells[index].style.marginRight = '10px';
+       // }
+    }
+}
+//
+
 const init = () => {
     const darkmode = JSON.parse(localStorage.getItem('darkmode'));
     document.body.classList.add(darkmode ? 'dark' : 'light');
@@ -40,6 +62,8 @@ const init = () => {
     const game = getGameInfo();
 
     document.querySelector('#btn-continue').style.display = game ? 'grid' : 'none';
+
+    initGameGrid();
 }
 
 init();
